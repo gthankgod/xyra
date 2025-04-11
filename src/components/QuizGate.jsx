a58c70f7d512d9ae124a9326e23f88f9a870bf8c
ThankGod George <thankgod@flutterwavego.com>
2025-04-12 00:52:28 +0100

    fix fe 2

--- src/components/QuizGate.jsx
+++ src/components/QuizGate.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import IntroForm from "./introForm";
+import IntroForm from "./IntroForm";
 import axios from "axios";
 
 const QuizGate = () => {
